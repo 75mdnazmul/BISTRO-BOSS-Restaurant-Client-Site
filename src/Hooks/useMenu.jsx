06ae5819e1e1dc7fb667b316b732ae2db911e7eb
75mdnazmul <75mdnazmulhasan.com>
2024-07-30 10:03:23 +0600

    server api link added

--- src/Hooks/useMenu.jsx
+++ src/Hooks/useMenu.jsx
@@ -4,30 +4,14 @@ const useMenu = () => {
     const [menu, setMenu] = useState([])
     const [loading, setLoading] = useState(true)
     useEffect(() => {
-        fetch('https://bistro-boss-restaurant-server-site-e9rnwul5z.vercel.app/Foods', {
-            headers: {
-                'Authorization': 'Bearer YOUR_API_TOKEN'
-            }
-        })
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(`HTTP error! Status: ${res.status}`);
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-
-                setMenu(data)
+        fetch('https://bistro-boss-restaurant-server-site-one.vercel.app/Foods')
+            .then(res => res.json())
+            .then(data =>
+                setMenu(data),
                 setLoading(false)
-            })
-            .catch(err => {
-                console.error('Fetch error: ', err);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    }, []);
-return [menu, loading]
+            )
+    }, [])
+
+    return [menu, loading]
 }
 export default useMenu;
